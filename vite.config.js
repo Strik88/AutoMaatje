0f542753057a6b1022f8b7de5f3f12c47dad72ee
Ian Strik <jouw_email@voorbeeld.com>
2025-04-09 21:59:38 +0200

    Fix assets and script paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/AutoMaatje/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
